cc25039b177f5a1f53b12d8cac4d4257baf9a5c1
mm <max.michel@km-h.at>
2022-07-03 15:46:41 +0200

    Added data set class for unaired data

--- Datasets.py
+++ Datasets.py
@@ -1,36 +1,53 @@
 from torch.utils.data import Dataset
 from skimage import io
 import torch.nn
+from random import randint
 
 import os
 import torch
 
-class DancingDataset(Dataset):
+class UnpairedData(Dataset):
 
-    # TODO  figure out dataloading
-
-    def __init__(self, orig_path, transform=None):
+    def __init__(self, A_path, B_path, BtoA = False, transform=None):
 
         self.transform = transform
 
-        self.imageNames = os.listdir(orig_path)
-        self.orig_path = orig_path
+        self.ANames = os.listdir(A_path)
+        self.A_path = A_path
+
+        self.BNames = os.listdir(B_path)
+        self.B_path = B_path
+        self.len_A = len(self.ANames)
+        self.len_B = len(self.BNames)
 
     def __len__(self):
-        return len(self.imageNames)
+        return max(self.len_A, self.len_B)
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        img_name = self.imageNames[idx]
+        if idx >= self.len_A:
+            A_name = self.ANames[randint(0,self.len_A-1)]
+        else:
+            A_name = self.ANames[idx]
 
-        img_orig = io.imread(os.path.join(self.orig_path,
-                                img_name))
-        img_orig = img_orig.transpose(2,0,1)
-        img_orig = img_orig/256
+        if idx >= self.len_B:
+            B_name = self.BNames[randint(0,self.len_B-1)]
+        else:
+            B_name = self.BNames[idx]
 
-        sample = {'image': img_orig}
+        img_A = io.imread(os.path.join(self.A_path,
+                                A_name))
+        img_A = img_A.transpose(2,0,1)
+        img_A = img_A/256
+
+        img_B = io.imread(os.path.join(self.B_path,
+                                       B_name))
+        img_B = img_B.transpose(2, 0, 1)
+        img_B = img_B / 256
+
+        sample = {'image_A': img_A, 'image_B': img_B}
 
         if self.transform:
             sample = self.transform(sample)
